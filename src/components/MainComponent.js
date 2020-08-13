504431b70ca03571b3660609f2f594e106a2bbab
havasneeze <havasneeze@gmail.com>
2020-08-13 13:27:07 -0700

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,40 +1,42 @@
 import React, { Component } from "react";
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
-import Home from './HomeComponent';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { CAMPSITES } from "../shared/campsites";
 
 class Main extends Component {
-
   constructor(props) {
-      super(props);
-      this.state = {
-          campsites: CAMPSITES
-      };
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+    };
   }
 
   render() {
+    const HomePage = () => {
+      return <Home />;
+    };
 
-      const HomePage = () => {
-          return (
-              <Home />
-          );
-      };
-
-      return (
-          <div>
-              <Header />
-              <Switch>
-                  <Route path='/home' component={HomePage} />
-                  <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
-                  <Redirect to='/home' />
-              </Switch>
-              <Footer />
-          </div>
-      );
-      }
-    }   
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/directory"
+            render={() => <Directory campsites={this.state.campsites} />}
+          />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
+}
 export default Main;
