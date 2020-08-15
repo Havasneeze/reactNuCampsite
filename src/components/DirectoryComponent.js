c70d809c5b0b0ebdcd3c1dd8b8e20c0e7bb8e46e
havasneeze <havasneeze@gmail.com>
2020-08-14 21:39:47 -0700

    Breadcrumbs

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
 import { Link } from 'react-router-dom';
 
-function RenderDirectoryItem({campsite}) {
+function RenderDirectoryItem({campsite, onClick}) {
   return (
       <Card>
           <Link to={`/directory/${campsite.id}`}>
@@ -26,9 +26,21 @@ function Directory(props) {
 
   return (
     <div className="container">
-      <div className="row">{directory}</div>
+        <div className="row">
+            <div className="col">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Directory</BreadcrumbItem>
+                </Breadcrumb>
+                <h2>Directory</h2>
+                <hr />
+            </div>
+        </div>
+        <div className="row">
+            {directory}
+        </div>
     </div>
-  );
+);
 }
 
 export default Directory;
